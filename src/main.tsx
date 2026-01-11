24449c30769fb385685ae1adb87da5198a8abedc
Okpara007 <chinemeremokpara93@gmail.com>
2026-01-11 16:50:19 +0100

    removed redundant GA code

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { AuthProvider } from './context/AuthContext'
-import { initGA } from './utils/analytics.ts'
-
-initGA();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
